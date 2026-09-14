55f4759e87d480ae117f8bb10da86134a2072cdb
paulnguyendesigns <paulxnguyen12@gmail.com>
2026-09-13 22:27:12 -0700

    adding companies file

---
+++ src/companies.py
@@ -0,0 +1,5 @@
+COMPANIES = [
+    {"name": "Stripe", "source": "greenhouse", "identifier": "stripe"},
+    {"name": "Palantir", "source": "lever", "identifier": "palantir"},
+    {"name": "Ramp", "source": "ashby", "identifier": "ramp"},
+]
